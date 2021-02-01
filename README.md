68bccba0f64ed56bd5c473e4c3d4945bcbaf7f31
David Window <david.window@kakaocorp.com>
2021-02-01 11:31:28 +0900

    초기 세팅
    - 프로젝트 clone
    - .gitignore 생성
    - 프로젝트 생성

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # swift-w5-shopping
 모바일 5주차 쇼핑 저장소
+
+
+
+### 초기 세팅
+
+- 프로젝트 clone
+- .gitignore 생성
+- 프로젝트 생성
+
---
+++ StoreApp/StoreApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoreApp
+//
+//  Created by Hochang Lee on 2021/02/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
